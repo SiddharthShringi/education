5fa972180b9c97ec9f3915021a1b2678618ddbca
BlackSamsung <siddharthshringi@gmail.com>
2019-01-18 17:39:44 +0530

    make literacy-rate api_view

--- djangoreactproject/urls.py
+++ djangoreactproject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mapdata import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/literacy_rate', views.literacy_rate_data)
 ]
--- mapdata/views.py
+++ mapdata/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import requests
 
 # Create your views here.
+
+
+# api view to get literacy data
+
+@api_view(['GET',])
+def literacy_rate_data(request):
+    data_url = "https://data.gov.in/node/100084/datastore/export/json"
+    response = requests.get(data_url)
+    if request.method == 'GET':
+        return Response(data = response.json())
